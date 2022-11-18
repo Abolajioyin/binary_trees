2c5db8c570c757608b2535e96dfd4a80fb281ff1
Abolajioyin <bolajioyinloye@yahoo.com>
2022-11-17 20:19:10 -0800

    Bug fixes

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,54 +1,48 @@
 #include "binary_trees.h"
-int recurse_for_complete(const binary_tree_t *tree, size_t index, size_t size);
-size_t binary_tree_size(const binary_tree_t *tree);
+
 /**
- * binary_tree_is_complete - check if tree is complete
- * @tree: pointer to root of tree
- *
- * Return: 1 if complete; 0 if incomplete
+ * binary_tree_rotate_left - left rotate binary tree
+ * @tree: pointer to the root node
+ * Description: 22. Rotate left
+ * Return: pointer to new root node
  */
-int binary_tree_is_complete(const binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	/* declare and initialize pointers */
+	binary_tree_t *new = NULL, *root = NULL;
 
-	return (recurse_for_complete(tree, 0, binary_tree_size(tree)));
-}
-/**
- * recurse_for_complete - use array indexing to track node position
- * @tree: pointer to root of tree
- * @index: index of node in conceptual array
- * @size: number of nodes in tree
- *
- * Return: 1 if complete; 0 if incomplete
- */
-int recurse_for_complete(const binary_tree_t *tree, size_t index, size_t size)
-{
-	int left, right;
+	/* if root node is NULL */
+	if (tree == NULL)
+		return (tree);
 
-	if (!tree)
-		return (1);
+	/* if root node has no children */
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (tree);
 
-	if (index >= size)
-		return (0);
-
-	left = recurse_for_complete(tree->left, (2 * index) + 1, size);
-	right = recurse_for_complete(tree->right, (2 * index) + 2, size);
-
-	if (left == 1 && right == 1)
-		return (1);
+	/* set pointer to new root */
+	new = tree->right;
+	/* set pointer to new root's left child */
+	tree->right = new->left;
+	/* if a new root's left child is valid, then */
+	/* new root will become the tree */
+	if (new->left != NULL)
+		new->left->parent = tree;
+	/* swap parents */
+	new->parent = tree->parent;
+	/* if there's no parent, current root will point to new root */
+	/* else if current root's parent has a left child, point it to new root */
+	/* all else, point current root's parent's right child to new root */
+	if (tree->parent == NULL)
+		root = new;
+	else if (tree == tree->parent->left)
+		tree->parent->left = new;
 	else
-		return (0);
-}
-/**
- * binary_tree_size - measure size of binary tree
- * @tree: tree to measure
- *
- * Return: size
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree)
-		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
-	return (0);
+		tree->parent->right = new;
+	/* move tree to the left of new */
+	new->left = tree;
+	tree->parent = new;
+	/* if current root is not NULL, return it, otherwise return new root*/
+	if (root)
+		return (root);
+	return (new);
 }
